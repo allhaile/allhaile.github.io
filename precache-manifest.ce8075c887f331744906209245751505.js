d90b5b13e9a75f8f9f1fb8129e0377de4b7d7ce3
Haile Shavers <haileshavers@Hailes-MacBook-Pro.local>
2020-06-24 20:00:39 -0700

    Updates

---
+++ precache-manifest.ce8075c887f331744906209245751505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2191418e191b599c13b3247c7979b6",
+    "url": "/haile-site/index.html"
+  },
+  {
+    "revision": "0c5dc0f04d8cecbf6448",
+    "url": "/haile-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05366446294539eabaa7",
+    "url": "/haile-site/static/js/2.ad572bae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haile-site/static/js/2.ad572bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5dc0f04d8cecbf6448",
+    "url": "/haile-site/static/js/main.c9def29f.chunk.js"
+  },
+  {
+    "revision": "100ff2b117f264eaa43e",
+    "url": "/haile-site/static/js/runtime-main.f39fd618.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/haile-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
